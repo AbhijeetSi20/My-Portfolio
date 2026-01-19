2bffe7dad33a1089403dad7c69223d109ff88536
Abhijeet Singh <155611134+AbhijeetSi20@users.noreply.github.com>
2026-01-19 21:07:05 +0530

    Set HashRouter basename to PUBLIC_URL

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename={process.env.PUBLIC_URL}>
       <App />
     </HashRouter>
   </React.StrictMode>,
